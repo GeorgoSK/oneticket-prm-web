06adfb52515018c5de923cfda1adefde143688fa
Juraj Kovac <juraj.kovac@somemove.cz>
2019-10-16 00:12:13 +0200

    Completed direct insertion mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready(() => {
 	var stationList = [];
 	$.getJSON("js/sr70.json", function(data) {
-		stationList = data.stations;
+		stationList = data.stations.map((i) => {
+			i.value = i.label;
+			return i;
+		});
 		$('.input-fields input').autocomplete({minLength: 0, source: stationList});
 	});
 	$('#search-results').hide();
